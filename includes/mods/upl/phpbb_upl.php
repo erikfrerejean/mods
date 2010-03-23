c082a41676ddaa913130cf6c488d8ae60cb437a8
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-03-23 11:41:23 +0100

    Initial setup

---
+++ includes/mods/upl/phpbb_upl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package   UPL
+ * @copyright 2010 Erik Frèrejean ( erikfrerejean@phpbb.com ) http://www.erikfrerejean.nl
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ *            GNU General Public License
+ * @version   Release: @package_version@
+ *
+ * Minimum Requirement: PHP 5.1.x
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (!defined('PHPBB_ROOT_PATH')) define('PHPBB_ROOT_PATH', $phpbb_root_path);
+if (!defined('PHP_EXT')) define('PHP_EXT', $phpEx);
+
+/**
+ *
+ */
+class phpbb_upl
+{
+	public function __construct()
+	{
+		
+	}
+}
