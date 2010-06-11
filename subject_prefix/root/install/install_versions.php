3910f27871f87af19a88ec27c951712c0b64b779
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-06-11 13:28:47 +0200

    Keep MPV happy

--- subject_prefix/root/install/install_versions.php
+++ subject_prefix/root/install/install_versions.php
@@ -10,6 +10,14 @@
 * Minimum Requirement: PHP 5.1.0
 */
 
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
 /*
 * The array of versions and actions within each.
 * You do not need to order it a specific way (it will be sorted automatically), however, you must enter every version, even if no actions are done for it.
