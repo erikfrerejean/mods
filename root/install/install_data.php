c19fa582fe7ac9365e333dfda1839e90969bd503
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-07-25 23:09:48 +0200

    Repackaged after MPV

--- root/install/install_data.php
+++ root/install/install_data.php
@@ -127,5 +127,6 @@ $versions = array(
 		),
 	),
 	'1.2.0-rc1'	=> array(),	// No database changes
-	'1.2.0'		=> array(), // No database changes
+	'1.2.0'		=> array(),	// No database changes
+	'1.2.1'		=> array(),	// No database changes
 );
